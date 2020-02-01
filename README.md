67572ac96a5d6ff6bef82fd503a482df109db028
koby5i <bobkoby5i@xdeq.com>
2020-02-02 00:12:31 +0100

    initial commit project created

---
+++ README.md
@@ -0,0 +1,14 @@
+# spring-cloud-intro-config-server Example
+
+
+Things to be checked before starting the application:
+
+
+
+## Application flow:
+ 
+* Client -> ConfigServer -> github
+
+
+## To do:
+
---
+++ config-client/src/test/java/com/koby5i/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koby5i.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
